d439033c19d4d1b8073fb3fc5fe23b130f6cf942
Kim-YH-handong <82792998+Kim-YH-handong@users.noreply.github.com>
2023-07-29 11:33:43 +0900

    Category Move

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -136,7 +136,7 @@ class _LoginPageState extends State<LoginPage> {
                           Navigator.pushReplacement(
                             context,
                             MaterialPageRoute(
-                              builder: (context) => BottomNavigatorPage(),
+                              builder: (context) => CategoryRegisterPage(),
                             ),
                           );
                         }
